8a8d3bbd330abb8f2a74f812c7dc13965ab68726
AbdullahErzincanli <abdullaherzincanli6497@gmail.com>
2023-01-30 09:50:14 +0300

    coinlist_cardview_and_filtered_coinlist

--- core/src/main/java/com/hamzacanbaz/core/items/CoinListItem.kt
+++ core/src/main/java/com/hamzacanbaz/core/items/CoinListItem.kt
@@ -7,6 +7,7 @@ import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -23,57 +24,80 @@ import kotlin.math.roundToInt
 fun CoinItem(item: Coin){
     // TODO BURADA Coin olmamali onun yerine string, int seklinde olmali
 
-    var coinValue:Float = item.priceUsd.toFloat()
-    coinValue = ((coinValue * 10000.0).roundToInt() / 10000.0).toFloat()
-    var coinChangeRate:Float = item.changePercent24Hr.toFloat()
-    coinChangeRate = ((coinChangeRate * 100.0).roundToInt() / 100.0).toFloat()
-    val coinValueStr:String = coinValue.toString()
-    val coinChangeStr:String = coinChangeRate.toString()
+    val coinValue = decimalPointChanger(item.priceUsd,4.0)
+    val coinChangeRate = decimalPointChanger(item.changePercent24Hr,2.0)
+    val colorChangeValue = changeColor(coinChangeRate.toFloat())
 
     Card(shape = RoundedCornerShape(8.dp),
-    backgroundColor = Color.DarkGray,
-    modifier = Modifier.fillMaxWidth().wrapContentHeight().padding(start = 8.dp, end = 8.dp, bottom = 8.dp, top = 8.dp)) {
+        backgroundColor = Color.DarkGray,
+        modifier = Modifier.fillMaxWidth().wrapContentHeight().padding(start = 8.dp, end = 8.dp, bottom = 8.dp, top = 8.dp)) {
 
-        Row() {
+        Row {
             Image(painter = rememberImagePainter(data = "https://coinicons-api.vercel.app/api/icon/" + item.symbol.lowercase(),
                 builder = {
                     scale(Scale.FILL)
                     transformations(CircleCropTransformation())
                 }),
-                modifier = Modifier
-                    .height(60.dp)
-                    .width(60.dp).padding(16.dp),
+                modifier = Modifier.height(80.dp)
+                    .fillMaxWidth().padding(16.dp).weight(1f).align(Alignment.CenterVertically),
                 contentDescription = null)
-            
-            Column {
+
+            Column( modifier = Modifier.wrapContentHeight()
+                .fillMaxWidth().padding(16.dp).weight(2f).align(Alignment.CenterVertically)) {
                 Text(text = item.name,
                     style = MaterialTheme.typography.subtitle1,
-                fontWeight = FontWeight.Bold,
-                    color = Color.White
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White,
+                    textAlign = TextAlign.Left,
+                    modifier = Modifier.fillMaxWidth()
                 )
                 Text(text = item.symbol,
                     style = MaterialTheme.typography.subtitle1,
                     fontWeight = FontWeight.Bold,
-                    color = Color.White
+                    color = Color.White,
+                    textAlign = TextAlign.Left,
+                    modifier = Modifier.fillMaxWidth()
                 )
             }
-            Spacer(modifier = Modifier.width(16.dp))
-            Column {
-                Text(text = "$coinValueStr $",
-                    style = MaterialTheme.typography.caption,
+
+            // Spacer(modifier = Modifier.width(16.dp))
+
+            Column( modifier = Modifier.wrapContentHeight()
+                .fillMaxWidth().padding(16.dp).weight(2f).align(Alignment.CenterVertically)) {
+                Text(text = "$coinValue $",
+                    style = MaterialTheme.typography.subtitle1,
                     fontWeight = FontWeight.Bold,
                     color = Color.White,
-                    textAlign = TextAlign.End
+                    textAlign = TextAlign.Right,
+                    modifier = Modifier.fillMaxWidth()
                 )
-                Text(text = "%$coinChangeStr",
-                    style = MaterialTheme.typography.caption,
+                Text(text = "%$coinChangeRate",
+                    style = MaterialTheme.typography.subtitle1,
                     fontWeight = FontWeight.Bold,
-                    color = Color.White,
-                    textAlign = TextAlign.End
+                    color = colorChangeValue,
+                    textAlign = TextAlign.Right,
+                    modifier = Modifier.fillMaxWidth()
                 )
             }
         }
+
     }
 }
 
+private fun decimalPointChanger(coinValueString:String, pointCount:Double):String{
+    val coinValue = (((coinValueString.toFloat() * Math.pow(10.0,pointCount)).roundToInt() /Math.pow(10.0,pointCount)).toFloat()).toString()
+    return coinValue
+}
+
+private fun changeColor(coinChangeRate:Float):Color{
+    var colorChangeValue:Color =  Color.Green
+    if(coinChangeRate<0){
+        colorChangeValue = Color.Red
+    }else if(coinChangeRate == 0f){
+        colorChangeValue = Color.White
+    }else{
+        colorChangeValue = Color.Green
+    }
+    return  colorChangeValue
+}
 
