271886d74e8ba5a4c2a08f608ed60ce570f15ad3
hamzacanbaz <hamza168canbaz48@gmail.com>
2023-02-02 17:07:11 +0300

    Add Coin Detail Screen

--- domain/src/main/java/com/hamzacanbaz/domain/model/coinDetail/Coin.kt
+++ domain/src/main/java/com/hamzacanbaz/domain/model/coinDetail/Coin.kt
@@ -1,16 +1,16 @@
 package com.hamzacanbaz.domain.model.coinDetail
 
 data class Coin(
-    val changePercent24Hr: String = "",
-    val explorer: String = "",
-    val id: String = "",
-    val marketCapUsd: String = "",
-    val maxSupply: String = "",
-    val name: String = "",
-    val priceUsd: String = "",
-    val rank: String = "",
-    val supply: String = "",
-    val symbol: String = "",
-    val volumeUsd24Hr: String = "",
-    val vwap24Hr: String = ""
+    val changePercent24Hr: String? = "",
+    val explorer: String? = "",
+    val id: String? = "",
+    val marketCapUsd: String? = "",
+    val maxSupply: String? = "",
+    val name: String? = "",
+    val priceUsd: String? = "",
+    val rank: String? = "",
+    val supply: String? = "",
+    val symbol: String? = "",
+    val volumeUsd24Hr: String? = "",
+    val vwap24Hr: String? = ""
 )
